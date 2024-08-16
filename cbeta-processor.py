c04c12e0b857afc193619690e02e92dd74c59a38
hwaomboy <hwaomboy@gmail.com>
2024-08-16 15:45:03 +0900

    클립보드 사용

--- cbeta-processor.py
+++ cbeta-processor.py
@@ -21,6 +21,10 @@ def process_text(content):
 st.title('CBETA 텍스트 처리')
 st.text("이 앱은 CBETA 등의 텍스트를 입력받아서 표점 부호를 빈칸으로 바꾸고 중국/일본식 한자를 한국식 한자로 바꿉니다.")
 
+# 세션 상태 초기화
+if 'processed_content' not in st.session_state:
+    st.session_state.processed_content = None
+
 input_option = st.radio(
     "입력 방식을 선택하세요:",
     ('텍스트 직접 입력', '파일 업로드')
@@ -34,28 +38,26 @@ else:
         content = uploaded_file.getvalue().decode("utf-8")
 
 if st.button('텍스트 처리') and 'content' in locals() and content.strip():
-    processed_content = process_text(content)
-    
+    st.session_state.processed_content = process_text(content)
+
+if st.session_state.processed_content:
     st.subheader("처리된 텍스트:")
-    st.code(processed_content)
-    # st.text_area("처리된 텍스트 (아래 '복사하기' 버튼을 사용하세요):", processed_content, height=200, key='processed_text')
+    st.text_area("처리된 텍스트:", st.session_state.processed_content, height=200, key='processed_text')
     
-    # col1, col2 = st.columns(2)
+    # 처리된 텍스트를 다운로드할 수 있게 합니다
+    output = io.BytesIO()
+    output.write(st.session_state.processed_content.encode('utf-8'))
+    st.download_button(
+        label="텍스트 파일로 다운로드",
+        data=output.getvalue(),
+        file_name="processed_text.txt",
+        mime="text/plain"
+    )
     
-    # # 처리된 텍스트를 다운로드할 수 있게 합니다
-    # output = io.BytesIO()
-    # output.write(processed_content.encode('utf-8'))
-    # st.download_button(
-    #     label="텍스트 파일로 다운로드",
-    #     data=output.getvalue(),
-    #     file_name="processed_text.txt",
-    #     mime="text/plain"
-    # )
-    
-    # # 클립보드에 복사 기능
-    # if st.button('텍스트 복사'):
-    #     st.code(processed_content)
-    #     st.success('위의 코드 블록에서 "Copy" 버튼을 클릭하여 텍스트를 복사하세요.')
+    # 클립보드에 복사 기능
+    if st.button('텍스트 복사'):
+        clipboard.copy(st.session_state.processed_content)
+        st.success('텍스트가 클립보드에 복사되었습니다.')
 
 else:
     st.info("텍스트를 입력하거나 파일을 업로드한 후 '텍스트 처리' 버튼을 클릭하세요.")
